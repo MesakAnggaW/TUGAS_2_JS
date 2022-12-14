9fe5f75deb44873182ccc6220be3a94d1a1a9d16
wccXpfhc <89910124+MesakAnggaW@users.noreply.github.com>
2022-12-14 22:08:27 +0700

    Mengerjakan Tugas 2 Javascript

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+let satu = 1980;
+let dua = "Tipe data string";
+let tiga = false;
+let empat = null;
+let lima;
+let enam = ["tipe", "data", "array", 23]
+
+console.log(satu);
+console.log(dua);
+console.log(tiga);
+console.log(empat);
+console.log(lima);
+console.log(enam);
